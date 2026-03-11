33b64ccb6294bff7fcbeaf9243462c89ac18b058
alice-carr <alice.bc.carr@gmail.com>
2026-03-11 17:17:02 +0000

    fix broken homepage

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1773224164',resources: [
+    cacheName: 'chirpy-1773249321',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

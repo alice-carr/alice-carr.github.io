efd47130ab2bf68b352606d789c53f36b26598be
alice-carr <alice.bc.carr@gmail.com>
2026-03-11 09:12:28 +0000

    update contact me

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1773219267',resources: [
+    cacheName: 'chirpy-1773220155',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

713921dafda3294d645283c96f45895ddd908c7c
alice-carr <alice.bc.carr@gmail.com>
2026-03-21 09:01:42 +0000

    update cv and homepage images

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1773249797',resources: [
+    cacheName: 'chirpy-1774083638',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

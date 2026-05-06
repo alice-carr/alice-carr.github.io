30db0cf9b1c076d0e80599602b98c8d03cfbe28b
alice-carr <alice.bc.carr@gmail.com>
2026-05-06 18:59:46 +0100

    update list of exhibitions

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1774083638',resources: [
+    cacheName: 'chirpy-1778090346',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
